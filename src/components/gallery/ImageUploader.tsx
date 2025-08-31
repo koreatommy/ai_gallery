b65b0b2d3071917b81678a107dc0daa526d32b09
eugene <eugene@example.com>
2025-08-31 21:10:16 +0900

    이미지 업로드에 저작권 경고 문구 추가

--- src/components/gallery/ImageUploader.tsx
+++ src/components/gallery/ImageUploader.tsx
@@ -239,6 +239,23 @@ export default function ImageUploader({ onUploadComplete, maxFiles = 10 }: Image
         </div>
       </Card>
 
+      {/* 저작권 경고 */}
+      <Card className="p-4 bg-amber-50 border-amber-200">
+        <div className="flex items-start space-x-3">
+          <div className="flex-shrink-0 mt-0.5">
+            <svg className="h-5 w-5 text-amber-600" fill="currentColor" viewBox="0 0 20 20">
+              <path fillRule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
+            </svg>
+          </div>
+          <div>
+            <h4 className="text-sm font-medium text-amber-800">저작권 주의사항</h4>
+            <p className="text-sm text-amber-700 mt-1">
+              업로드할 때 저작권에 위배되지 않도록 주의하세요. 본인이 직접 촬영하거나 제작한 이미지만 업로드해주세요.
+            </p>
+          </div>
+        </div>
+      </Card>
+
       {/* 드래그 앤 드롭 영역 */}
       <Card
         {...getRootProps()}
